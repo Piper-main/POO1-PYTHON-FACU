d5496765aeb30d3298a198f7f4ed07bc698b8536
Piper <elnaruuuu@gmail.com>
2026-08-23 17:42:53 -0300

    2 ejercicios nuevos

---
+++ Funciones/numero_expasivos.py
@@ -0,0 +1,11 @@
+"""
+* Así, los primeros números expansivos son:
+* 1 -> "un uno": 11
+* 11 -> "dos unos": 21
+* 21 -> "un dos, un uno": 1211
+* 1211 -> "un uno, un dos, dos uno": 111221
+
+Se pide programar: 
+1. una función "expand" que, dado un arreglo con las cifras de un número expansivo genere el siguiente
+2. una función "list2num" que, dado un arreglo con las cifras de un número expansivo, devuelva el número correspondiente
+"""
