3015520e5b81d87c46f3e0f2edad6fa4dcf5cb87
Piper <elnaruuuu@gmail.com>
2026-08-27 20:32:51 -0300

    fin de la explicacion de la clase(class)

--- clases/pokemon.py
+++ clases/pokemon.py
@@ -6,14 +6,21 @@ class Pikachu:             # declaramos la clase con la palabra reservada "class
         self.nombre = nombre                 # creamos la variables de instancia "self.nombre"
         self.nivel = nivel
         self.salud = salud
+    """CREAMOS UN METODO DENTRO DE LA CLASE"""
+    """UN METODO ES UNA ACCION O COMPORTAMIENTO QUE PUEDE TENER LE OBJETO"""
+    def atacar(self):
+        print(f" pikachu ataca y genera {self.nivel/4} de daño")
 
-"""AHORA CREAMOS EL OBJETO PARA USAR SUS ATRIBUTOS"""
+
+    """AHORA CREAMOS EL OBJETO PARA USAR SUS ATRIBUTOS"""
 # creo el objeto poniendole un nombre y es = al nombre de la clase y le puedo poner los valores
 # que utilizaran sus variables de instancia.
 Pikachu1 = Pikachu('Felipe',1000,5000)
 Pikachu2 = Pikachu('Nahuel', 1500, 4000)
 
 
+print(f"el pikachu llamada {Pikachu1.nombre} ataca.")
+Pikachu1.atacar()  #llamamos a la funcion atacar con el nombre de objeto y tomara sus valores para atacar
 
-print(Pikachu1.tipo,Pikachu1.nombre,Pikachu1.nivel)
-print(Pikachu2.tipo,Pikachu2.nombre,Pikachu2.nivel)
+print(f"el pikachu llamada {Pikachu2.nombre} ataca.")
+Pikachu2.atacar()
