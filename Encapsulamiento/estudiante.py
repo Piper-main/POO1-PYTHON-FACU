17206c2a677d9c9821f4c525e00d3d8c65361048
Piper <elnaruuuu@gmail.com>
2026-08-28 20:01:32 -0300

    nuevos ejercicios

---
+++ Encapsulamiento/estudiante.py
@@ -0,0 +1,6 @@
+"""
+Realizar un programa que conste de una clase llamada Estudiante, 
+que tenga  como atributos el nombre y la nota del alumno. 
+Definir los métodos para inicializar sus atributos, imprimirlos y 
+mostrar un mensaje con el resultado de la nota y si ha aprobado o no
+"""
