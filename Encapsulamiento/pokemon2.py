31cddfe04958db3bc424e1fdfeee624fe8f5bada
Piper <elnaruuuu@gmail.com>
2026-08-27 21:14:22 -0300

    encapsulamiento sin arrancar

--- Encapsulamiento/pokemon2.py
+++ Encapsulamiento/pokemon2.py
@@ -3,8 +3,8 @@ class Pikachu:             # declaramos la clase con la palabra reservada "class
 
 
     def __init__(self, nombre,nivel,salud,voltaje_max,amperaje_max,color):  
-        self.nombre = nombre                                               
-        self.nivel = nivel
+        self._nombre = nombre                                               
+        self.__nivel = nivel
         self.salud = salud
         self.voltaje_maximo = voltaje_max
         self.apmperaje_maximo = amperaje_max
@@ -17,5 +17,7 @@ Pikachu1 = Pikachu('nahuel', 780,100,6,2,'amarillo')
 
 
 
-Pikachu1.nivel = 900
-print(f" El pikachu llamada {Pikachu1.nombre} es de color {Pikachu1.color} y tiene un nivel de {Pikachu1.nivel} y es de tipo {Pikachu1.tipo}")
+Pikachu1.__nivel = 900    # esto si por que el pokemon evoluciono y subio de nivel
+Pikachu1.tipo = 'fuego' #esto no deberia poder hacerse. no puede cambiar la clase nunca.
+
+print(f" El pikachu llamada {Pikachu1._nombre} es de color {Pikachu1.color} y tiene un nivel de {Pikachu1.__nivel} y es de tipo {Pikachu1.tipo}")
