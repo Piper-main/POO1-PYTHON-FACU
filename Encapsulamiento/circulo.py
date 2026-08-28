9f0ae46d891e45373f7a1a74cf89fd7e50edf760
Piper <elnaruuuu@gmail.com>
2026-08-28 20:03:00 -0300

    circulo

---
+++ Encapsulamiento/circulo.py
@@ -0,0 +1,12 @@
+"""Implementar una clase que modele un círculo, del que se desea manipular (obtener y cambiar):
+* radio
+* diámetro
+* perímetro
+* área
+"""
+
+class circulo:
+
+    def __init__(self, radio)-> None:
+        self.__radio = radio
+        
