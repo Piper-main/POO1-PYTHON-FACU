6af4aca4e2b36fa5fd342b528e80ee5f88a40e66
Piper <elnaruuuu@gmail.com>
2026-09-16 20:39:58 -0300

    restaurante de desafios

--- practicas/restaurante.py
+++ practicas/restaurante.py
@@ -7,7 +7,7 @@ class CategoriaPlato(Enum):
     POSTRE=3
     BEBIDA=4
 
-class Plato():
+class Plato:
 
     def __init__(self, nombre, precio, categoria):
         self.__nombre = nombre
@@ -31,5 +31,44 @@ class Plato():
     def set_nombre(self, nuevo_nombre):
          self.__nombre = nuevo_nombre
 
-    def set_categoria(self, nueva_categoria)
+    def set_categoria(self, nueva_categoria):
          self.__categoria = nueva_categoria
+
+class Pedido:
+  
+
+     def __init__(self):
+         self.__lista_plato = []
+
+     def agregar_plato(self, plato):
+         self.__lista_plato.append(plato)
+
+     def calcular_total(self):
+         total = 0    
+         for p in self.__lista_plato:
+            total = total + p.get_precio()
+            
+         return total
+
+     def ticket(self):
+          
+
+          for p in self.__lista_plato:
+               nombre = p.get_nombre()
+               precio = p.get_precio()
+               categoria = p.get_categoria()
+
+               print(nombre)
+               print(precio)
+               print(categoria)
+
+          print(self.calcular_total())
+             
+               
+plato1 = Plato('Fideos', 4575, 'ENTRADA')
+plato2 = Plato('Milanesas', 9000, 'PRICIPAL')
+
+pedido1 = Pedido()
+pedido1.agregar_plato(plato1)
+pedido1.agregar_plato(plato2)
+pedido1.ticket()
