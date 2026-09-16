fe46913b03b2ab8835e02b2c9fe4fdb9990a7c1a
Piper <elnaruuuu@gmail.com>
2026-09-15 21:06:13 -0300

    Estudiante2

---
+++ practicas/estudiante2.py
@@ -0,0 +1,22 @@
+
+
+class Estudiante:
+
+    def __init__(self, n, no):
+        self.nombre = n
+        self.nota = no
+
+    def mostrar_datos(self):
+        print(f"el nombre es {self.nombre} y la nota es {self.nota}")
+
+    def aprobado(self):
+        if self.nota >= 4:
+            print("El estudiante aprobo")
+        elif self.nota < 4:
+            print("El estudiante no aprobo")
+
+Estudiante1 = Estudiante("Nahuel", 2)
+Estudiante1.mostrar_datos()
+Estudiante1.aprobado()
+    
+        
